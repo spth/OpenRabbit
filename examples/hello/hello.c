051424ca57e9100e097ae13b0e10d597514a24ce
Philipp Klaus Krause <pkk@spth.de>
2020-10-13 14:16:08 +0200

    Port examples to more RCMs.

--- examples/hello/hello.c
+++ examples/hello/hello.c
@@ -17,6 +17,12 @@
 #elif defined(RCM3209)
 #define SERIAL_DIVIDER_19200 36
 
+#elif defined(RCM3319)
+#define SERIAL_DIVIDER_19200 36
+
+#elif defined(RCM3750)
+#define SERIAL_DIVIDER_19200 18
+
 #elif defined(RCM4110)
 #define SERIAL_DIVIDER_19200 48 // 29.5 MHz crystal , thus clock running at full speed despite clock doubler off on startup.
 
